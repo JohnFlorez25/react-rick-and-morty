2f3dc830a2d3c835342131091e6d4541661ded04
John Faber <john.florez25@gmail.com>
2020-05-26 09:41:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from  'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      loading : true,
+      data : undefined,
+      error : null
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        const characters = res.data.results
+        this.setState( {
+          data : characters,
+          loading: false
+        })
+        console.log(this.state.data);
+      })
+  }
+
+  render(){
+
+    if(this.state.loading === true 
+        && 
+        this.state.data === undefined){
+      return(
+        <h1>Cargando....</h1>
+      )
+    }
+
+    return(
+      this.state.data.map( character => {
+        return(
+          <div key={character.id}>
+            <h3>
+              {character.name}
+            </h3>
+            <h4>
+              {character.species}
+            </h4>
+          </div>
+        )
+      })
+    )
+  }
+
 }
 
 export default App;
